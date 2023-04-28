2387ff1a93df8ef349120d810481a3a48f3fc0d5
Your Name <hamzabenarfa4@gmail.com>
2023-04-28 18:14:46 +0100

    added scss , added header , made it respondive still buggy tho

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,49 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react';
+import './navbar.css';
+import { Link } from 'react-router-dom';
+import { IoDiamond,IoMenu } from "react-icons/io5";
 
 export default function Navbar() {
+  const [toggle, setToggle] = useState(false);
+  const headerRef = useRef(null);
+  const [headerWidth, setHeaderWidth] = useState(0);
+
+  useEffect(() => {
+    setHeaderWidth(headerRef.current.offsetWidth);
+  }, [toggle]);
+
   return (
-    <div>Navbar</div>
-  )
+    <>
+      <section className="navBarSection">
+        <header className="header flex" ref={headerRef}>
+          <div className="logoDiv">
+            <Link to='/' className="logo flex">
+              <h1><IoDiamond className='icon' />Luxuria Travel</h1>
+            </Link>
+          </div>
+          {(headerWidth > 768 & !toggle )&& (
+            <div className="navBar">
+              <ul className="navLists flex">
+                <li className="navItem">
+                  <Link to='/' className="navLink">Home</Link>
+                </li>
+                <li className="navItem">
+                  <Link to='/' className="navLink">Order</Link>
+                </li>
+                <li className="navItem">
+                  <Link to='/' className="navLink">Tour</Link>
+                </li>
+                <button className='btn'>
+                  <Link to='/'>LogIn</Link>
+                </button>
+              </ul>
+            </div>
+          )}
+          <div className="toggleNavbar">
+            <IoMenu className='icon' onClick={()=>setToggle(!toggle)}/>
+          </div>
+        </header>
+      </section>
+    </>
+  );
 }
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,30 +1,12 @@
 import React from 'react'
-import './Home.css'
-import useAxios from '../../hooks/useAxios';
-import useFetch from '../../hooks/useFetch';
 
 export default function Home() {
-  const url = "https://localhost:8000/api";
-  const { data } = useAxios(
-   `${url}/user` ,'get'
-  );
-  console.log(data);
+ 
 
 
 
   return (
     <div>
-        {
-            data && data.map((users) =>{
-                return(
-                    <div key={users.id}>*user
-                         <h1>{users.nom}</h1>mail
-                      <h1>{users.mail}</h1>
-
-                    </div>
-                )
-            })
-        }
 
     </div>
 
